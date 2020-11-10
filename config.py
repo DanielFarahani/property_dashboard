9cab93a51d7284d7cb17648ee3888b6e0f742ae8
danielfarahani <danfarahani@gmail.com>
2020-11-10 19:08:26 +1100

    mod'

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ class ProductionConfig(Config):
     # PostgreSQL database
     SQLALCHEMY_DATABASE_URI = '{}://{}:{}@{}:{}/{}'.format(
         config('DB_ENGINE'  , default='postgresql'    ),
-        config('DB_USERNAME', default='appseed'       ),
+        config('DB_USERNAME', default='df'       ),
         config('DB_PASS'    , default='pass'          ),
         config('DB_HOST'    , default='localhost'     ),
         config('DB_PORT'    , default=5432            ),
